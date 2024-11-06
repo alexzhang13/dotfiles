46cf972978b2396078b2b14feb3671c49d4f96f3
alexzhang13 <alex.lx.zhang@gmail.com>
2024-11-05 18:15:53 -0800

    Update config.lua

--- vim/.config/lvim/config.lua
+++ vim/.config/lvim/config.lua
@@ -11,16 +11,58 @@ lvim.log.level = "warn"
 lvim.format_on_save = true
 
 -- keybindings
-lvim.leader = "space"
+-- lvim.leader = "space"
 -- lvim.keys.normal_mode["gk"] = "<Cmd>lua vim.lsp.buf.hover()<CR>"
+-- lvim.lsp.buffer_mappings.normal_mode['K'] = lvim.lsp.buffer_mappings.normal_mode['3k']
+lvim.lsp.buffer_mappings.normal_mode['K'] = nil
+lvim.keys.normal_mode['K'] = "<Cmd>echo Okay!<CR>"
+
 lvim.lsp.buffer_mappings.normal_mode['gk'] = { vim.lsp.buf.hover, "Show documentation" }
 
-lvim.lsp.buffer_mappings.normal_mode['K'] = nil
-lvim.keys.normal_mode["J"] = "3j"
+lvim.keys.normal_mode["K"] = false
 lvim.keys.normal_mode["K"] = "3k"
-lvim.keys.visual_mode["J"] = "3j"
 lvim.keys.visual_mode["K"] = "3k"
+lvim.keys.normal_mode["J"] = "3j"
+lvim.keys.visual_mode["J"] = "3j"
 lvim.keys.normal_mode["|"] = ":vsplit<CR>"
 lvim.keys.normal_mode["-"] = ":split<CR>"
 -- colors
 -- additional plugins
+lvim.plugins = {
+  {
+    "nosduco/remote-sshfs.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    opts = {
+      -- Refer to the configuration section below
+      -- or leave empty for defaults
+    },
+  },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    build = "make", -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
+    opts = {
+      -- add any opts here
+      provider="copilot",
+    },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below is optional, make sure to setup it properly if you have lazy=true
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  }
+}
+
+lvim.builtin.telescope.on_config_done = function(telescope)
+  pcall(telescope.load_extension, "remote-sshfs")
+  -- any other extensions loading
+end
